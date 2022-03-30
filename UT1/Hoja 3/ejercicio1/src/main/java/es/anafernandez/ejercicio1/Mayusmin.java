5a74341d9968f31d10c0a4df2d75ea23a2e6b973
@AnaFernandez <anabelen.fernandezsanmiguel@iesmiguelherrero.com>
2022-03-30 13:21:00 +0200

    hoja 3

--- UT1/Hoja 3/ejercicio1/src/main/java/es/anafernandez/ejercicio1/Mayusmin.java
+++ UT1/Hoja 3/ejercicio1/src/main/java/es/anafernandez/ejercicio1/Mayusmin.java
@@ -61,7 +61,6 @@ public class Mayusmin {
                 } else {
                     System.out.print(letra);
                 }
-
             }
             br.close();
 
--- UT1/Hoja 3/ejercicio3/src/main/java/es/anafernandez/ejercicio3/MenuFicheros.java
+++ UT1/Hoja 3/ejercicio3/src/main/java/es/anafernandez/ejercicio3/MenuFicheros.java
@@ -14,6 +14,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -43,7 +45,7 @@ public class MenuFicheros {
                 System.out.println("6- Salir");
                 opcion = teclado.nextInt();
                 File numeros = new File("numeros.dat");
-                int aleatorio;              
+                int aleatorio;
                 switch (opcion) {
                     case 1:
                         //grabar en el fichero personas.txt
@@ -54,17 +56,28 @@ public class MenuFicheros {
                     case 2:
                         br = new BufferedReader(new FileReader("personas.txt"));
                         String linea;
+                        String nombreC = recogerDatos();
                         Boolean encontrado = false;
                         while ((linea = br.readLine()) != null) {
-                            System.out.println(linea);
-                            if (linea.equalsIgnoreCase(recogerDatos())) {
+                            //System.out.println(linea);
+                            if (linea.equalsIgnoreCase(nombreC)) {
                                 System.out.println("Ese nombre ya existe" + linea);
                                 encontrado = true;
                             }
-                            else{
-                                System.out.println("el nombre no existe en nuestra base de datos");}
-                            break;
                         }
+                        //no funciona
+                        break;
+                    case 3:
+                        /*Se recoge por teclado un nombre de persona y se devuelve una lista con las personas del
+fichero que tienen ese nombre. Luego se imprime la lista.*/
+                        System.out.println("teclea nombre");
+                        String nombre = teclado.nextLine();
+                        teclado.nextLine();
+                        List<String> nombres = nombres(nombre);
+                        //System.out.println(nombres);
+
+                        break;
+
                 }
             } catch (UnsupportedEncodingException ex) {
                 System.out.println(ex.getMessage());;
@@ -73,7 +86,6 @@ public class MenuFicheros {
             } catch (IOException ex) {
                 System.out.println("error de E/S");
             }
-
         } while (opcion != 6);
     }
 
@@ -93,5 +105,35 @@ public class MenuFicheros {
         nombreCompleto = apellido.toUpperCase() + ", " + nombre.toUpperCase() + "\n";
         System.out.println(nombreCompleto);
         return nombreCompleto;
+    }
+
+    public static List<String> nombres(String nombre) {
+        BufferedReader br = null;
+        List<String> nombres = null;
+        try {
+            br = new BufferedReader(new FileReader("personas.txt"));
+            nombres = new ArrayList();
+            String linea = "";
+            Boolean encontrado = false;
+            while ((linea = br.readLine()) != null) {
+                String[] piezas = linea.split(",");
+                if (nombre.equalsIgnoreCase(piezas[1])) {
+                    nombres.add(piezas[1]);
+                    nombres.add("\n");
+                    encontrado = true;
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MenuFicheros.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MenuFicheros.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MenuFicheros.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return nombres;
     }
 }
